a6a0a3f7df858b8b66fa0aa73fdc694f7411476a
Syed Abdul Basit Shah <159904063+basitshah0@users.noreply.github.com>
2024-12-20 18:07:09 +0500

    SMIT-JavaScript-Assignment

---
+++ Javascript Assignments/ass1/chapterNo03.js
@@ -0,0 +1,32 @@
+// Chatpter no 03
+
+// Question no 01
+
+var age = 20
+
+alert(`I'm ${age} years old`)
+
+// Question no 02
+
+var number = 16
+
+alert(`You have visited this site ${number} times`)
+
+//Question no 03
+
+var birthYear = 2004
+
+document.write (`<h1> My birth Year is ${birthYear} </h1>`)
+
+var desc = "Data type of my declared variable is number"
+
+document.write (` <h1> ${desc} </h1>`)
+
+// Question no 04
+
+var visitorName = "Abdul Basit"
+var product = "T-Shirt"
+var quantity = 5
+var storeName = "J."
+
+document.write (`<h1> ${visitorName} ordered ${quantity} ${product}(s) on ${storeName} </h1>`)
---
+++ Javascript Assignments/ass2/chapterNo4.js
@@ -0,0 +1,56 @@
+// Assignment No 02
+
+// Chapter no 04
+
+//Question No 01
+
+// 1. Declare 3 variables in one statement.
+
+// Answer :-
+
+var myName = "Abdul Basit" , myAge = 20 , isStudent = true
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Answer :-
+
+var My_Variable;                        // Legal
+var $my_Variable;                       // Legal
+var _my_Variable;                       // Legal
+var My_Variable_Example;                // Legal
+var myVariableExample;                  // Legal
+
+// var 1My_Variable;                       //illegal
+// var @My_Variable;                       //illegal
+// var ++My_Variable;                      //illegal
+// var %My_Variable;                       //illegal
+// var #my_Variable;                       //illegal
+
+
+// 3. Display this in your browser
+
+
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// Answer :-
+
+document.write("<h1>Chapter no 04</h1>")
+document.write("<h1>Question no 3 </h1>")
+//a
+document.write ("<h1>Rules for naming JS variables </h1>")
+//b
+document.write ("Variable names can only contain alphabets ,numbers , $ and _ For example $my_1stVariable. </br>")
+//c
+document.write ("Variables must begin with a letter , $ or _. For example $name, _name or name </br>")
+//d
+document.write ("Variable names are case sensitive </br>")
+//e
+document.write ("Variable names should not be JS keywords </br>")
+
